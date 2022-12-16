a98e1508151568a4fd731c1cd346f5f489f9c8fd
aort <artashesano@icloud.com>
2022-12-16 18:19:04 +0400

    Merge colors.dart file with constants

--- lib/src/models/cell_theme.dart
+++ lib/src/models/cell_theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:verification_code_field/src/theme/colors.dart' as colors;
 
 // Default values
 part './constants.dart';
--- lib/src/models/constants.dart
+++ lib/src/models/constants.dart
@@ -1,13 +1,19 @@
 part of './cell_theme.dart';
 
-/// Cell theme default values
+// Default colors
+const primaryColor = Color(0xffFFFFFF);
+const backgroundColor = Color(0xff61727C);
+// TODO: unused, use or remove
+const errorColor = Color(0xffBD2A2E);
+
+// Cell theme default values
 const _height = 50.0;
 const _width = 40.0;
 const _borderWidth = 1.0;
-const _borderColor = colors.primaryColor;
+const _borderColor = primaryColor;
 const _borderRadius = 7.0;
-const _backgroundColor = colors.backgroundColor;
+const _backgroundColor = backgroundColor;
 const _textStyle = TextStyle(
-  color: colors.primaryColor,
+  color: primaryColor,
   fontWeight: FontWeight.bold,
 );
